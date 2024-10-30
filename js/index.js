e6869289af543c74a14e500b817784a0655b74b4
masanzashimba <simeonmasanza@gmail.com>
2024-10-30 07:27:16 +0100

    Ajout du dossier initial

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Selects all elements
+const burgerMenu = document.querySelector("#burger-menu");
+const sideBarMenu = document.querySelector("#side-bar");
+const mainSection = document.querySelector("#main-section");
+
+// Callback functions
+const handleAddStyle = (element, style) => {
+  element.classList.add(style);
+};
+
+const handleRemoveStyle = (element, style) => {
+  element.classList.remove(style);
+};
+
+// Add events
+
+burgerMenu.addEventListener("click", (event) => {
+  if (sideBarMenu.classList.contains("display-none")) {
+    handleRemoveStyle(sideBarMenu, "display-none");
+    handleRemoveStyle(mainSection, "w-100");
+  } else {
+    handleAddStyle(sideBarMenu, "display-none");
+    handleAddStyle(mainSection, "w-100");
+  }
+});
